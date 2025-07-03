8fa48e3e48105472d3404c5907e1893008b49bfe
Jaewook Lee <wookboy00@naver.com>
2025-07-03 16:01:20 +0900

    feat: nestjs module

---
+++ index.ts
@@ -0,0 +1,4 @@
+export * from './module';
+export * from './guard';
+export * from './service';
+export * from './decorator';
---
+++ module.ts
@@ -0,0 +1,23 @@
+import {KMSGuard} from "./guard";
+import {DynamicModule} from "@nestjs/common";
+import { KMSVerifier } from "./service";
+
+export interface KMSModuleOption {
+    agentURL?: string;
+}
+
+export class KMSModule {
+    static forRoot(option?: KMSModuleOption): DynamicModule {
+        return {
+            module: KMSModule,
+            providers: [
+                {
+                    provide: KMSVerifier,
+                    useValue: new KMSVerifier(option?.agentURL)
+                },
+                KMSGuard,
+            ],
+            exports: [KMSGuard, KMSVerifier]
+        }
+    }
+}
