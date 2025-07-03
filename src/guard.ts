1c89f213dff2dd86aeef2199ac68e37befe26e1b
Jaewook Lee <wookboy00@naver.com>
2025-07-03 17:37:47 +0900

    chore: guard가 던지는 에러 추가

--- src/guard.ts
+++ src/guard.ts
@@ -1,4 +1,11 @@
-import {CanActivate, ExecutionContext, Injectable, Inject, ForbiddenException} from "@nestjs/common";
+import {
+    CanActivate,
+    ExecutionContext,
+    Injectable,
+    Inject,
+    ForbiddenException,
+    BadRequestException
+} from "@nestjs/common";
 import * as requestIp from '@supercharge/request-ip';
 import {KMSVerifier} from "./service";
 
@@ -12,8 +19,11 @@ export class KMSGuard implements CanActivate {
         const request = context.switchToHttp().getRequest();
         const ip = requestIp.getClientIp(request);
         if (!ip) throw new ForbiddenException(); // 아이피 없는 경우
+        if (!request.headers.authorization) throw new BadRequestException();
+        if (!request.headers.authorization.startsWith('Bearer ')) throw new BadRequestException();
+        const token = request.headers.authorization.replace('Bearer ', '');
 
-        const [clientId, keyName] = await this.verifier.verify(request.headers.authorization, ip)
+        const [clientId, keyName] = await this.verifier.verify(token, ip)
         request.KMSClientId = clientId;
         request.KMSkeyName = keyName;
 
