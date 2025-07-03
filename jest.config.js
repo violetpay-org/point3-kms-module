591531e3496fe998debabb9f5cca442a9d7366a1
Jaewook Lee <wookboy00@naver.com>
2025-07-03 16:00:32 +0900

    chore: package setup

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testRegex: '(/__tests__/.*|(\.|/)(test|spec))\.(ts|js)x?$',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    '**/*.{ts,js}',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+  ],
+};
